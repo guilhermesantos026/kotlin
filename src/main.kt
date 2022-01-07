3266aec61ccbdd5f2f9ff3c8995ef925c4456274
Guilherme Fermino <guilherme@guilhermes-mbp.home>
2022-01-07 19:53:07 -0300

    Variáveis e template String

---
+++ src/main.kt
@@ -0,0 +1,8 @@
+fun main(){
+
+    val nome = "Guilherme Fermino" // Imutável
+    var idade = 26                 // Mutável
+
+    println("$nome" + " tem a idade de: $idade") // String template (Usamos o -> $ )
+
+}
