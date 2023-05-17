045bd2a2890f1387b6ae418e0803089ab9ea2d42
Guilherme Fermino <guilhermesantos026@gmail.com>
2023-05-17 00:14:46 -0300

    Created docker-compose file

--- src/main/kotlin/com/study/kotlin/controller/Hello.kt
+++ src/main/kotlin/com/study/kotlin/controller/Hello.kt
@@ -1,4 +1,4 @@
-package com.study.kotlin
+package com.study.kotlin.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
