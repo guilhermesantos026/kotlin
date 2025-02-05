c413ecd0dbfcf0c3d7ea98017f694fa2b027c53c
Guilherme Fermino <guilhermesantos026@gmail.com>
2025-02-05 00:25:39 -0300

    feat: Created the resource to find all and find by id

--- src/main/kotlin/com/fermino/forum/controller/TopicController.kt
+++ src/main/kotlin/com/fermino/forum/controller/TopicController.kt
@@ -2,10 +2,12 @@ package com.fermino.forum.controller
 
 import com.fermino.forum.domain.Topic
 import com.fermino.forum.service.TopicService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @RestController
 @RequestMapping("/topics")
@@ -14,5 +16,15 @@ class TopicController (private val topicService: TopicService) {
     @PostMapping
     fun create(@RequestBody topic: Topic){
         topicService.create(topic)
+    }
+
+    @GetMapping
+    fun findAll(): List<Topic> {
+        return topicService.findAll()
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): Topic {
+        return topicService.findById(id)
     }
 }
--- src/main/kotlin/com/fermino/forum/service/TopicService.kt
+++ src/main/kotlin/com/fermino/forum/service/TopicService.kt
@@ -9,4 +9,14 @@ class TopicService (var topics: List<Topic> = ArrayList()) {
     fun create(topic: Topic) {
         topics += topic
     }
+
+    fun findAll(): List<Topic> {
+        return topics
+    }
+
+    fun findById(id: Long): Topic {
+        return topics.stream().filter({
+            topic -> topic.id == id
+        }).findFirst().get()
+    }
 }
